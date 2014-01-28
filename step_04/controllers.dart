e836bfaaade54b95c2d94b99fca630ad2ce4976c
Ando Yasushi <andyjpn@gmail.com>
2014-01-29 01:20:05 +0900

    add step 04

---
+++ step_04/controllers.dart
@@ -0,0 +1,32 @@
+library phonecat;
+
+import 'package:angular/angular.dart';
+import 'package:logging/logging.dart';
+
+@NgController(
+  selector: '[phone-list]',
+  publishAs: 'ctrl'
+)
+class PhoneListCtrl {
+  List<Map> phones = [
+    {'name': 'Nexus S',
+     'snippet': 'Fast just got faster with Nexus S.',
+     'age': 1},
+    {'name': 'Motorola XOOM™ with Wi-Fi',
+     'snippet': 'The Next, Next Generation tablet.',
+     'age': 2},
+    {'name': 'MOTOROLA XOOM™',
+     'snippet': 'The Next, Next Generation tablet.',
+     'age': 3}
+  ];
+  String query = '';
+  String orderProp = 'age';
+}
+
+main() {
+  Logger.root.level = Level.FINEST;
+  Logger.root.onRecord.listen((LogRecord r) { print(r.message); });
+  Module phoneCatModule = new Module()
+    ..type(PhoneListCtrl);
+  ngBootstrap(module: phoneCatModule);
+}
