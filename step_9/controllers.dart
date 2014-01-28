2a134ff4f19db6123e0ee9fa274e21d0f6eaaa0a
Ando Yasushi <andyjpn@gmail.com>
2014-01-28 23:04:35 +0900

    add step 9

---
+++ step_9/controllers.dart
@@ -0,0 +1,43 @@
+library phonecat_controller;
+
+import 'package:angular/angular.dart';
+
+@NgController(
+  selector: '[phone-list]',
+  publishAs: 'ctrl',
+  map: const {
+    'phones': '=>phones'
+  }
+)
+class PhoneListCtrl {
+  Http _http;
+
+  List<Map> phones = [];
+  String query = '';
+  String orderProp = 'age';
+
+  PhoneListCtrl(Http this._http) {
+    this._http.get("./phones/phones.json").then((resp) {
+      this.phones = resp.data;
+    });
+  }
+}
+
+@NgController(
+  selector: '[phone-detail]',
+  publishAs: 'ctrl',
+  map: const {
+    'phone': '=>phone'
+  }
+)
+class PhoneDetailCtrl {
+  Http _http;
+  Map phone;
+
+  PhoneDetailCtrl(RouteProvider routeProvider, Http this._http) {
+    String phoneId = routeProvider.parameters['phoneId'];
+    this._http.get("./phones/$phoneId.json").then((resp) {
+      this.phone = resp.data;
+    });
+  }
+}
