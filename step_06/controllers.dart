274e524f61079217dddec22c49bed75cb36facff
Ando Yasushi <andyjpn@gmail.com>
2014-01-29 01:12:48 +0900

    add step 06

---
+++ step_06/controllers.dart
@@ -0,0 +1,34 @@
+library phonecat;
+
+import 'package:angular/angular.dart';
+import 'package:logging/logging.dart';
+
+@NgController(
+  selector: '[phone-list]',
+  publishAs: 'ctrl',
+  map: const {
+    'phones': '=>phones'
+  }
+)
+class PhoneListCtrl {
+  Http _http;
+
+  List<Map> phones = [];
+  String query = '';
+  String orderProp = 'age';
+
+  PhoneListCtrl(Http this._http) {
+    this._http.get("./phones/phones.json").then((resp) {
+      print(resp.data);
+      this.phones = resp.data;
+    });
+  }
+}
+
+main() {
+  Logger.root.level = Level.FINEST;
+  Logger.root.onRecord.listen((LogRecord r) { print(r.message); });
+  Module phoneCatModule = new Module()
+    ..type(PhoneListCtrl);
+  ngBootstrap(module: phoneCatModule);
+}
